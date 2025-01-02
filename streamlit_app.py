8b1d30834847827d225bd02c7b06c47825a85508
alp006 <56029833+alp006@users.noreply.github.com>
2025-01-02 16:40:00 +0100

    Update streamlit_app.py
    moved smoothiefroot_response into for statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,9 @@ if ingredients_list:
   ingredients_string = ''
   for x in ingredients_list:
     ingredients_string += x + ' '
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
   #st.write(ingredients_string)
 
   my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -34,6 +37,3 @@ if ingredients_list:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered, '+order_name+'!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
